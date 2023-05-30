4172f70d09aa9aea9fe67a92011ae09ca4fc23f1
clxdev <domhenin.517@gmail.com>
2023-05-30 10:12:41 -0400

    functioning code :: everything builds as expected

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ module "compute_base" {
   public_subnet_id_b = module.network_base.vpc_pub_sub_b
 }
 
-module "peering_connection_base" {
-  source = "./modules/aws/peering-connections-base"
+# module "peering_connection_base" {
+#   source = "./modules/aws/peering-connections-base"
 
 
-  vpc_id_a = module.network_base.vpc_id_a
-  vpc_id_b = module.network_base.vpc_id_b
-}
+#   vpc_id_a = module.network_base.vpc_id_a
+#   vpc_id_b = module.network_base.vpc_id_b
+# }
 
 module "network_base" {
   source = "./modules/aws/network-base"
